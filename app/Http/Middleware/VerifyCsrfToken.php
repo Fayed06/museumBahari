8fcafa7b1c4d77a827bcede5e9d7b7dac0f46999
Muhammad Rafly Alfayed <79348164+Fayed06@users.noreply.github.com>
2022-11-13 01:28:28 +0700

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        'https://museum-bahari.telenurse.web.id/api',
+        'https://museum-bahari.telenurse.web.id/api/tickets/order',
+        'https://museum-bahari.telenurse.web.id/api/link/submit',
     ];
 }
